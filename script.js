fce342fd59745b2ad8100fb2d624086ca9d79180
TlonUqbar <luismadrigal@duck.com>
2023-09-01 16:55:39 -0700

    feat: Add event listeners to buttons
    Calculater buttons or keys are now wired and input is received
    from them.
    The digits show up on the screen.
    The AC, C and backspace all work now.
    The Operation buttons not fully wired yet, but the input is received.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let number1, number2, operator;
+const smallScreen = document.querySelector(".intermediate");
+const mainScreen = document.querySelector(".result");
+const opSign = document.querySelector(".operator");
+const sign = document.querySelector(".sign");
+
+const digits = document.querySelectorAll(".digit");
+const functions = document.querySelectorAll(".function");
+const operations = document.querySelectorAll(".operation");
+
+let number1 = "";
+let number2 = "";
+let operator;
 
 
 const operate = (operator, number1, number2=0 ) => {
@@ -10,6 +21,7 @@ const operate = (operator, number1, number2=0 ) => {
       if( number2 === 0) return " 💣 😱 🤯 💔 🪦 ";  
       return number1 / number2;
     case "%": return parseFloat(number1 / 100);
+    case "±": return ;
     default: break;
 
   }
@@ -18,6 +30,57 @@ const operate = (operator, number1, number2=0 ) => {
 
 
 
+digits.forEach( (digit) => {
+  digit.addEventListener("click", (e) => {
+    if(number1.length >= 12) return;
+    console.log("key", e.target.textContent);
+    number1 += e.target.textContent;
+    mainScreen.textContent = number1;
+  })
+})
 
 
+functions.forEach( (functn) => {
+  functn.addEventListener("click", (e) => {
+    console.log("key", e.target.textContent );
+    switch(e.target.textContent) {
+      case "AC":
+        mainScreen.textContent = "0";
+        opSign.textContent = "";
+        number1 = "";
+        number2 = "";
+        operator = "";
+        break;
+      case "C" :
+        number1 = "";
+        mainScreen.textContent = "0";
+        break;
+      case "⌦" :
+        number1 = number1.slice(0, -1);
+        mainScreen.textContent = number1;
+        if(number1 == "") mainScreen.textContent = "0";
+      default: break;
+    } 
+  })
+});
+
+
+operations.forEach( (op) => {
+  op.addEventListener("click", (e) => {
+    console.log("Op: ", e.target.textContent);
+    operator = e.target.textContent;
+    opSign.textContent = operator;
+
+    if( operator === "±") {
+      opSign.textContent = "";
+      if(number1 == "0") {
+        number1 = "-0";
+      }else {
+        number1 = number1 * -1;
+      }
+      
+      mainScreen.textContent = number1;
+    }
+  })
+})
 
