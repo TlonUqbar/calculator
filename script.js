30882bded8d320bd092e8431a3dd3d0efaa5ec86
TlonUqbar <luismadrigal@duck.com>
2023-09-25 17:39:49 -0700

    refactor: Remove unnecessary comments and console

--- script.js
+++ script.js
@@ -20,19 +20,6 @@ let result = 0;
 let sign = "+";
 
 
-const showOverlay = (e) => { overlay.style.display = "block"; }
-const hideOverlay = (e) => { overlay.style.display = ''; }
-
-
-// EventListeners
-exit.addEventListener("click", hideOverlay);
-help.addEventListener("click", showOverlay);
-window.addEventListener("keydown", keypad );
-keys.forEach( (key) => { key.addEventListener("click", keypad ); });
-
-
-
-
 function operate( operator, number1, number2){
   switch(operator) {    
     case "+" : return add(number1, number2);
@@ -52,6 +39,13 @@ function resets(code){
   }
 }
 
+// There are serveral possible input conditions and influences behavior
+// This makes things more complicated since, we have to account for them.
+// Condition 1: Nothing is stored in variables, so input is just stored.
+// Condition 2a: Expected inputs - number and operator and a number and operator already stored
+// Condition 2b: Unexpected inputs - only operator and number and operators already stored
+// Because of this, it is not possible to always have just one function for the operation.
+
 function keypad(e){
   e.preventDefault();
 
@@ -69,10 +63,9 @@ function keypad(e){
   if( e.code === "Escape" || e.code === "Delete" || e.code === "Backspace" ) resets(e.code);
 
   // Filter keyboad input, limited to Numpad Numbers, Operations and the % sign.  Enter is mapped to sign change
-  if( !(e.key.includes("%") || e.key === "Enter"  || (e.code.includes("Numpad") && !e.key.match(exclude) ) ) ) return;
+  if( !(e.key.includes("%") || e.key === "Enter" || (e.code.includes("Numpad") && !e.key.match(exclude) ) ) ) return;
 
   // get input and separate operators from digits
-  
   if( e.key.match(/[-|+|*|=|\/|%|Enter]/)  ) {
     if( reg1 && !op1 && e.key !== '=') { op1 = e.key; return; }
     if( reg1 === '' && op1 === '' && reg2 === ''  && op2 === ''){
@@ -102,13 +95,13 @@ function keypad(e){
           default: break;
         }
       } else if( !input && e.key){
-        // mostly edge cases, both inputs and one operator
+        // mostly edge cases, one input and two operators
         switch(e.key){
           case "+" :
           case "-" :
           case "*" :
           case "/" : return condition2BasicB(e);
-          case "Enter" : return signChange(e);
+          case "Enter" : return signChange2(e);
           case "%" : return condition2PercentB(e);
           case "=" : return condition2EqualsB(e);
           default: break;
@@ -121,15 +114,14 @@ function keypad(e){
     if(input === '' && e.key.match(/[+|-|*|\/]/));
     if( (input === '' || input === "0") && e.key === "." ) input = "0."; // pad small decimals with 0   
     if(input.includes(".") && e.key == ".") return;  // Allow only one '.' for Decimals/floats
-    if( input.match(/-0[0-9]*/) && !input.includes(".")) input = input.replace("-0", '-');  // remove the 0 padding after 1st number
+    if( input.match(/-0[0-9]*/) && !input.includes(".")) input = input.replace("-0", '-'); // remove the 0 pad 
     if(input.length >= 12) return; // Maximum number of digits allowed
     input = ( input === "0" ) ?  e.key : input + e.key;
     updateMainScreen(input);   
   }
 }
 
-
-
+// Display Modifying Functions
 const allClear = () => {
   mainScreen.textContent = '';
   smallScreen.textContent = '';
@@ -156,7 +148,6 @@ const backspace = () => {
   mainScreen.textContent = (input === '') ? 0 : input;
 }  
 
-
 const clearSmallScreen = () => {
   smallScreen.textContent = '';
   opSign.textContent = '';
@@ -168,12 +159,11 @@ const updateSmallScreen = (operator, inputs) => {
 }
 
 const updateMainScreen = (value) => {  
-  if( !value ) return;  // if empty just leave
+  if( !value ) return;
   mainScreen.textContent = value;
 }
 
-
-// individual operation functions
+// operation functions
 const add = (number1, number2) => {
   return Number(number1) + Number(number2);
 }
@@ -187,7 +177,6 @@ const multiply = (number1, number2) => {
 }
 
 const divide = (number1, number2) => {
-  // handle divide by 0 and display message
   if( number2 === 0) {
     smallScreen.textContent = "🙄 ➗ 0️⃣ 🤦‍♀️";
     mainScreen.textContent = "😱💣🤯💔🪦";
@@ -201,13 +190,10 @@ const percent = (number) => {
   return Number(number) / 100;
 }
 
+// Decisions based on input conditions
 
-
-// Decisions 
-
-// With basic operators ( +, -, * and /)
+// Basic Opertors (+,-,*,/)
 const condition1Basic = (e) => {
-  console.log("B1a: input and basicOps", e.key);
   reg1 = Number(input);
   op1 = e.key;
   input = '';
@@ -215,9 +201,6 @@ const condition1Basic = (e) => {
 }
 
 const condition2BasicA = (e) => {
-  console.log("B2a: input and basicOps");
-  console.log("input: ", input);
-  console.log("in ",reg1, reg2, op1, op2, result, input, e.key);
   reg2 = Number(input);
   op2 = e.key;
   result = operate(op1, reg1, reg2);
@@ -228,18 +211,12 @@ const condition2BasicA = (e) => {
   op2 = '';
   updateSmallScreen(op1, result);
   input = '';
-  console.log("out", reg1, reg2, op1, op2, result, input, e.key);
 }
 
 // Maybe combine these 2, only diff seems to be reg2 value, a conditional will work
-
 const condition2BasicB = (e) => {
-  console.log("B2b: no input and basicOps");
-  console.log("input: ", input);
-  console.log("in ",reg1, reg2, op1, op2, result, input, e.key);
   reg2 = reg1;
   op2 = e.key;
-  console.log(op1, reg1, reg2, op2);
   result = operate(op1, reg1, reg2);
   result = checkOveflow(result);
   reg1 = result;
@@ -248,144 +225,121 @@ const condition2BasicB = (e) => {
   op2 = '';
   updateSmallScreen(op1, result);
   input = '';
-  console.log("out", reg1, reg2, op1, op2, result, input, e.key);
 }
 
-
-
+// Equals
 const condition1Equals = (e) => {
-  console.log("E1a: input and '=' ", e.key);
   result = Number(input);
   updateMainScreen(result);
   input = '';
 }
 
 const condition2EqualsA = (e) => {
-  console.log("E2a: input and '=' ");
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
-  let template = '';
   reg2 = Number(input);
   op2 = e.key;
-  template = `${reg1} ${op1} ${reg2}`;
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
+  let template = `${reg1} ${op1} ${reg2}`;
   result = operate(op1, reg1, reg2);
+  result = (result === 0) ? "0" : result;
+  
   if( !(op1 === '/' && reg2 === 0) ){ 
     updateSmallScreen(op2, template);
     result = checkOveflow(result);
-    reg1 = result;
+    reg1 = Number(result);
   } else{
     reg1 = '';
   }
+
   op1 = '';
   reg2 = '';
   op2 = '';
-  
   updateMainScreen(result);
   result = '';
   input = '';
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
 }
 
 const condition2EqualsB = (e) => { 
-  console.log("E2b: no input and '=' ");
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
-  let template = '';
   reg2 = reg1;
-  template = `${reg1} ${op1} ${reg2}`;
+  let template = `${reg1} ${op1} ${reg2}`;
   result = operate(op1, reg1, reg2);
   result = checkOveflow(result);
   reg1 = result;
   reg2 = '';
   updateSmallScreen(e.key, template);
   updateMainScreen(result);
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
 }
 
+// Percent
 const condition1Percent = (e) => {
-  console.log("P1a: input and '%' ", e.key);
-  console.log("%", result, input);
   result = percent(input);
   result = checkOveflow(result);
-  input = result;
-  console.log("%", result, input);
+  input = result.toString();
   updateMainScreen(result);
 }
 
 const condition2PercentA = (e) => {
-  console.log("P2a: input and '%' ");
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
-  let template;
-
   reg2 = Number(input)/100;
+
   if(op1.match(/[+|-]/)){
-    console.log("+,-");
     result = ( reg2) * reg1
   } else {
-    console.log('*,/');
     result = reg2;
   }
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
+
   op2 = e.key;
-  template = `${reg1} ${op1} ${reg2}`;
   op2 = '';
   reg2 = '';
-  // updateSmallScreen(e.key, template)
-  // updateMainScreen(result);
   input = result.toString();
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
 }
 
 const condition2PercentB = (e) => {
-  console.log("P2b: no input and '%' ");
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
-  let template; 
-  
   if(input === '') input = reg1.toString();
   reg2 = input/100;
   if(op1.match(/[+|-]/)){
-    console.log("+,-");
     result = reg1 + reg2;
   } else {
-    console.log('*,/');
     result = reg1 * reg2;
   }
 
-  template = `${reg1} ${op1} ${reg2}`;
   input = reg2.toString();
   reg2 = '';
   op2 = '';
-  
-  console.log(reg1, reg2, op1, op2, result, input, e.key);
 }
 
-
+// Sign Change
 const signChange = (e) => {
-  console.log("SC1a: '+/-' ", e.key);
-  console.log("+/- in", reg1, reg2, op1, op2, result, input, e.key, sign);
-  
+  genSignChange(e);
+  result = input;
+  updateMainScreen(input);
+}
+
+const signChange2 = (e) => {
+  input = reg1.toString();
+  genSignChange(e);
+  input = checkOveflow(input);
+  updateMainScreen(input);
+  result = Number(input);
+  reg1 = result;
+  op1 = '';
+  input = '';
+  clearSmallScreen();
+}
+
+const genSignChange = (e) => {
   if ( (input === "" || input === "0") && sign === "+") {
     input = "-0"
   } else if ( input.includes(".") && !input.includes("-")){
     input = "-"+input;
   } else if ( input.includes(".") && input.includes("-")){ 
     input = input.replace("-", "");
-  }else {
+  } else {
     input = (input * -1).toString();
   }
   sign = (sign === "+") ?  "-" : sign = "+";
   opSign.textContent =  ''; //"±";
-
-  result = input;
-  // input = '';  
-  updateMainScreen(input);
-  console.log("+/- out", reg1, reg2, op1, op2, result, input, e.key);
 }
 
-
-
-const checkOveflow = (results) => {
-  console.log(results);
-  // is this in exponential form?
+// Check that the answer is within the 12 digits limit
+const checkOveflow = (results) => {  
   if( results.toString().includes("e+") || results.toString().includes("e-") ){
     if( typeof results === "number" ) {
       results = results.toExponential(6);
@@ -397,27 +351,22 @@ const checkOveflow = (results) => {
 
   // check the result so it won't oveflow 
   if( results.toString().length > 12 ) {
-    console.log("res length", results.toString().length );
     let whole = results.toString().split(".")[0];
     let fractions = results.toString().split(".")[1];
     if( !fractions ) fractions = "0";
     if ( whole.length > fractions.length ) {
-      console.log("whole", whole.length);
       results = Number(results);
       results = results.toPrecision(whole.length - 8);
     } else {
-      console.log("fraction", fractions.length, results);
       if( results.toString().includes("e+") || results.toString().includes("e-") ){
         results = Number(results);
         results = results.toPrecision(fractions.length - 8);
       } else {
-        console.log("This?");
-        // result = Number(result);
         if(results.toString().includes("e+") || results.toString().includes("e-") ){
           results = Number(result);
         } else {
-        results = results.toLocaleString('fullwide', { minimumFractionDigits:0, maximumFractionDigits:10, 
-                      minimumSignificantDigits:1, maximumSignificantDigits:10, userGrouping:false });
+        results = results.toLocaleString('fullwide', { minimumFractionDigits:0, maximumFractionDigits:8, 
+                      minimumSignificantDigits:1, maximumSignificantDigits:10, userGrouping:false })
         }
       }
     }
@@ -425,8 +374,12 @@ const checkOveflow = (results) => {
   return results;
 }
 
+// Toggle Help Overlay
+const showOverlay = (e) => { overlay.style.display = "block"; }
+const hideOverlay = (e) => { overlay.style.display = ''; }
 
-
-
-
-
+// EventListeners
+exit.addEventListener("click", hideOverlay);
+help.addEventListener("click", showOverlay);
+window.addEventListener("keydown", keypad );
+keys.forEach( (key) => { key.addEventListener("click", keypad ); });
